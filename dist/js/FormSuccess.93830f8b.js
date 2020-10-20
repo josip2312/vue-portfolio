ece23458cc3709b3bda3325ff8e5500266275701
Josip Ivancic <ivancicjosip073@gmail.com>
2020-10-20 15:27:33 +0200

    deploy

--- dist/js/FormSuccess.93830f8b.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["FormSuccess"],{e96c:function(e,o,n){"use strict";n.r(o);var s=function(){var e=this,o=e.$createElement,n=e._self._c||o;return n("div",[n("h1",[e._v("You're Awesome! Thank you for your submission!")]),n("router-link",{attrs:{to:"/"}},[n("button",[e._v("Back to form")])])],1)},t=[],u={name:"SubmissionSuccess"},r=u,c=n("2877"),i=Object(c["a"])(r,s,t,!1,null,null,null);o["default"]=i.exports}}]);
-//# sourceMappingURL=FormSuccess.93830f8b.js.map
